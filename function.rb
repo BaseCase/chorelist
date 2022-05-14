849f0732cea1601fa2f442dbd5b01507073d705b
Casey Brant <casey@caseybrant.com>
2022-05-14 16:56:54 -0500

    Notion API wrapper for creating new entries in the running list db

--- function.rb
+++ function.rb
@@ -17,6 +17,12 @@ Chore = Struct.new(
   keyword_init: true
 )
 
+Assignment = Struct.new(
+  :id,
+  :name,
+  keyword_init: true
+)
+
 
 class Notion
   NOTION_API_SECRET = ENV['NOTION_API_SECRET']
@@ -25,10 +31,22 @@ class Notion
   MASTER_DB_URL = "#{NOTION_API_BASE}/databases/#{MASTER_LIST_DATABASE_ID}"
   RUNNING_LIST_DATABASE_ID = ENV['RUNNING_LIST_DATABASE_ID']
   RUNNING_DB_URL = "#{NOTION_API_BASE}/databases/#{RUNNING_LIST_DATABASE_ID}"
+  PAGES_URL = "#{NOTION_API_BASE}/pages"
 
   def self.get_master_database_metadata
     response = HTTParty.get(
       MASTER_DB_URL,
+      headers: {
+        'Authorization' => "Bearer #{NOTION_API_SECRET}",
+        'Notion-Version' => '2022-02-22'
+      })
+
+    JSON.parse response.body, symbolize_names: true
+  end
+
+  def self.get_running_list_metadata
+    response = HTTParty.get(
+      RUNNING_DB_URL,
       headers: {
         'Authorization' => "Bearer #{NOTION_API_SECRET}",
         'Notion-Version' => '2022-02-22'
@@ -60,6 +78,28 @@ class Notion
         assignees: row[:properties][:Assignees][:multi_select].map { |ass| ass[:name] },
         notes: "",
       )
+    end
+  end
+
+  def self.create_assignment(assignment)
+    res = HTTParty.post(
+      PAGES_URL,
+      headers: {
+        'Authorization' => "Bearer #{NOTION_API_SECRET}",
+        'Notion-Version' => '2022-02-22',
+        'Content-Type' => 'application/json'
+      },
+      body: {
+        parent: { "database_id" => RUNNING_LIST_DATABASE_ID },
+        properties: {
+          "Name" => { "title" => [{ "text" => { "content" => assignment.name }}]}
+        }
+      }.to_json
+    )
+
+    unless res.success?
+      puts "Error trying to create database entry via Notion API"
+      puts res
     end
   end
 end
