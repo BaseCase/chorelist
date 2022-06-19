673ea1a152c3cc438922a2aa1fc929e55ac7c310
Casey Brant <casey@caseybrant.com>
2022-06-19 15:14:23 -0500

    assignment rotation and weekly due date setting

--- lib/chore.rb
+++ lib/chore.rb
@@ -3,14 +3,37 @@ require 'date'
 class Chore
   attr_reader :frequency
 
-  def initialize(frequency: :weekly, assignees: [])
+  def initialize(frequency: :weekly, assignees: [], assignments: [])
     @frequency = frequency
     @assignees = assignees
+    @assignments = assignments
   end
 
   def latest_assignment
-    due = Date.new
-    @_latest_assignment ||= Assignment.new(person: @assignees.first, due_date: due)
+    if @assignments.empty?
+      today = Date.today
+      days_until_next_sunday = 7 - today.wday
+      due = today + days_until_next_sunday
+      @assignments << Assignment.new(person: @assignees.first, due_date: due)
+    end
+
+    @assignments.sort_by(&:due_date).last
+  end
+
+  def next_assignment
+    latest_person_idx = @assignees.index(latest_assignment.person)
+    next_person_idx = (latest_person_idx + 1) % @assignees.length
+    next_person = @assignees[next_person_idx]
+
+    next_due = one_week_after(latest_assignment.due_date)
+
+    Assignment.new(person: next_person, due_date: next_due)
+  end
+
+  private
+
+  def one_week_after(date)
+    date + 7
   end
 end
 
--- spec/chore_spec.rb
+++ spec/chore_spec.rb
@@ -1,3 +1,4 @@
+require 'date'
 require 'chore'
 
 RSpec.describe Chore do
@@ -6,9 +7,53 @@ RSpec.describe Chore do
     expect(chore.frequency).to eq :weekly
   end
 
-  it 'has a latest assignment with person responsible and due date' do
-    chore = Chore.new(assignees: ["Casey", "Elayne"])
-    expect(chore.latest_assignment.person).to eq "Casey"
-    expect(chore.latest_assignment.due_date).to be_instance_of Date
+  describe '#latest_assignment' do
+    it 'has a latest assignment with person responsible and due date' do
+      chore = Chore.new(assignees: ["Casey", "Elayne"])
+      expect(chore.latest_assignment.person).to eq "Casey"
+      expect(chore.latest_assignment.due_date).to be_instance_of Date
+    end
+
+    it 'takes a list of previous Assignments and finds the latest one' do
+      very_old = Date.new(2010, 05, 12)
+      kinda_old = Date.new(2020, 01, 01)
+      recent = Date.today
+
+      assignments = [very_old, recent, kinda_old].map { |date|
+        Assignment.new(person: "Casey", due_date: date)
+      }
+
+      chore = Chore.new(assignees: ["Casey"], assignments: assignments)
+
+      expect(chore.latest_assignment.due_date).to eq recent
+    end
+
+    it 'makes upcoming Sunday the first due date when not given a previous Assignment' do
+      creation_date = Date.new(2022, 06, 21)
+      the_next_sunday = Date.new(2022, 06, 26)
+      allow(Date).to receive(:today).and_return creation_date
+
+      chore = Chore.new(assignees: ["Casey"])
+
+      expect(chore.latest_assignment.due_date).to eq the_next_sunday
+    end
+  end
+
+  describe '#next_assignment' do
+    it 'rotates the assignee' do
+      chore = Chore.new(assignees: ["Casey", "Elayne"])
+      expect(chore.latest_assignment.person).to eq "Casey"
+      expect(chore.next_assignment.person).to eq "Elayne"
+    end
+
+    it 'sets the due date one week after the latest assignment' do
+      today = Date.today
+      one_week_later = today + 7
+      latest_assignment = Assignment.new(person: "Casey", due_date: today)
+      chore = Chore.new(assignees: ["Casey"], assignments: [latest_assignment])
+
+      expect(chore.latest_assignment.due_date).to eq today
+      expect(chore.next_assignment.due_date).to eq one_week_later
+    end
   end
 end
