6ae538e5857e9899a02d20702b1a06688e9daa08
Casey Brant <casey@caseybrant.com>
2022-06-19 22:03:41 -0500

    to_json for Assignment, in the shape Notion expects

--- lib/assignment.rb
+++ lib/assignment.rb
@@ -1,2 +1,16 @@
+require 'json'
+
 Assignment = Struct.new(:person, :due_date, :chore_id, :done, keyword_init: true) do
+  def to_json
+    {
+      "Chore" => { "relation" => [{ "id" => chore_id }]},
+      "Due" => { "date" => { "start" => due_date.iso8601 }},
+      "Assignee" => {
+        "right_text" => [
+          { "type" => "text", "text" => { "content" => person }}
+        ]
+      },
+      "Done" => { "checkbox" => done },
+    }.to_json
+  end
 end
--- spec/assignment_spec.rb
+++ spec/assignment_spec.rb
@@ -1,4 +1,27 @@
+require 'json'
 require 'assignment'
 
 RSpec.describe Assignment do
+  describe '#to_json' do
+    it 'is JSON in the shape the Notion API wants it' do
+      assignment = Assignment.new(
+        person: "Casey",
+        due_date: Date.parse('2022-06-02'),
+        chore_id: "12345",
+        done: false
+      )
+      expect(assignment.to_json).to eq(
+        {
+          "Chore" => { "relation" => [{ "id" => "12345" }]},
+          "Due" => { "date" => { "start" => '2022-06-02' }},
+          "Assignee" => {
+            "right_text" => [
+              { "type" => "text", "text" => { "content" => "Casey" }}
+            ]
+          },
+          "Done" => { "checkbox" => false },
+        }.to_json
+      )
+    end
+  end
 end
